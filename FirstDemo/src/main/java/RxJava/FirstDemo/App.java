6f69a3bdb78a09e48c63205495f57efed1eee414
HDB <abc@gmail.com>
2020-05-09 18:14:39 -0700

    local commit

--- FirstDemo/src/main/java/RxJava/FirstDemo/App.java
+++ FirstDemo/src/main/java/RxJava/FirstDemo/App.java
@@ -2,13 +2,18 @@ package RxJava.FirstDemo;
 
 /**
  * Hello world!
+ * Local
  *
  */
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-
+		System.out.println("local");
 		System.out.println("hello from member 1");
 		System.out.println("hello from member 2");
+	}
+	
+	public void mLocal() {
+		String s="Local changes";
 	}
 }
