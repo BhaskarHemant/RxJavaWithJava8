32ec33dc2b91e29347df43bee7cbc46fea3f8c33
BhaskarHemant <64155349+BhaskarHemant@users.noreply.github.com>
2020-05-09 18:09:16 -0700

    Remote changes toApp.java file

--- FirstDemo/src/main/java/RxJava/FirstDemo/App.java
+++ FirstDemo/src/main/java/RxJava/FirstDemo/App.java
@@ -2,13 +2,18 @@ package RxJava.FirstDemo;
 
 /**
  * Hello world!
+ * remote
  *
  */
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-
+		System.out.println("remote");
 		System.out.println("hello from member 1");
 		System.out.println("hello from member 2");
+	}
+	
+	public void mRemote() {
+		String s="Remote changes";
 	}
 }
