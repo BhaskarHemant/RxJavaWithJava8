1dda730b33e01afe7fd89f6e91fc45e078a2ae39
HDB <abc@gmail.com>
2020-05-09 18:23:52 -0700

    resolved merge conflict

--- FirstDemo/src/main/java/RxJava/FirstDemo/App.java
+++ FirstDemo/src/main/java/RxJava/FirstDemo/App.java
@@ -3,17 +3,23 @@ package RxJava.FirstDemo;
 /**
  * Hello world!
  * Local
+ * remote
  *
  */
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("local");
+		System.out.println("remote");
 		System.out.println("hello from member 1");
 		System.out.println("hello from member 2");
 	}
 	
 	public void mLocal() {
 		String s="Local changes";
+	}
+	
+	public void mRemote() {
+		String s="Remote changes";
 	}
 }
